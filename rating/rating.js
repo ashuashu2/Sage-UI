b8e3c6eeecf48127faf2295a7a269a508f07c8c0
ashuashu2 <86846323+ashuashu2@users.noreply.github.com>
2022-07-07 01:39:28 +0530

    update features in rating file

---
+++ rating/rating.js
@@ -0,0 +1,72 @@
+var stars = document.querySelectorAll(".stars")
+var submit = document.querySelector(".rating-submit-button")
+var tryAgain= document.querySelector(".try-again-button")
+
+var output= document.querySelector(".output")
+var ratings = document.querySelector(".rating-div")
+
+    for (i = 0; i < stars.length; i++) {
+        stars[i].starValue = (i+1)
+        const arr1 = ["click", "mouseover", "mouseout"]
+      
+        arr1.forEach(function(e){
+            stars[i].addEventListener(e,showRating)
+            
+        })
+      
+    }
+
+
+submit.addEventListener("click",show)
+tryAgain.addEventListener("click",tryAgains)
+function show(){
+
+    ratings.innerHTML = `Thank-you for submiting your feedback`
+    tryAgain.style.visibility = "visible" 
+   
+
+}
+function tryAgains(){
+    window.location.reload()
+}
+
+
+
+
+function showRating(e){
+let type = e.type
+let starValue = this.starValue
+stars.forEach(function(element,index) {
+if(type==="click"){
+
+if(index < starValue){
+    output.innerHTML = `You rated ${starValue} star`
+
+
+
+
+
+}
+
+}
+if(type==="mouseover"){
+
+    if(index < starValue){
+    
+    
+    
+        element.classList.add("red")
+    } else{
+        element.classList.remove("red")
+    }
+    
+    }
+    if(type==="mouseout"){
+
+    
+            element.classList.remove("red")
+       
+        
+        }
+})
+}
